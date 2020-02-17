5ccb3b33bb13370585b6aa2b914cf609e75614c0
hildebrandom <hilderbrand2007@hotmail.com>
2020-02-16 21:34:55 -0500

    add application

---
+++ IncityClient.Prism/IncityClient.Prism/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IncityClient.Prism.Models;
+
+namespace IncityClient.Prism.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
